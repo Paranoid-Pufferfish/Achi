1065a9f51a43d25ea92cc4d40bf31beaa442b526
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2024-11-23 17:03:43 +0100

    TODO: Add terminal case test
    Signed-off-by: Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>

--- main.c
+++ main.c
@@ -1,25 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "achi_board.h"
 
+typedef struct tree {
+    int state[9];
+    struct tree *next[9];
+} tree;
+
+tree *initTree(tree *previous, int place, int turn, int n, int depth) {
+    if (n > depth)
+        return nullptr;
+    if (previous->state[place] != 0)
+        return nullptr;
+    tree *P = malloc(sizeof(tree));
+    for (int i = 0; i < 9; ++i) {
+        P->state[i] = previous->state[i];
+    }
+    P->state[place] = turn;
+    int t;
+    for (int i = 0; i < 9; ++i) {
+        if (turn == 1)
+            t = -1;
+        else
+            t = 1;
+        P->next[i] = initTree(P, i, t, n + 1, depth);
+    }
+    return P;
+}
+
+void outputTree(tree *T) {
+    for (int i = 0; i < 9; ++i) {
+        if (T->state[i] == 0)
+            printf(".");
+        else if (T->state[i] == 1)
+            printf("X");
+        else
+            printf("O");
+
+        if (i == 2 || i == 5 || i == 8)
+            printf("\n");
+        else
+            printf(" ");
+    }
+}
+
+tree makeTree() {
+    tree Tree;
+    for (int i = 0; i < 9; ++i) {
+        Tree.state[i] = 0;
+        Tree.next[i] = nullptr;
+    }
+    for (int i = 0; i < 9; ++i) {
+        Tree.next[i] = initTree(&Tree, i, 1, 1, 6);
+    }
+    return Tree;
+}
+
 int main(void) {
+    tree T = makeTree();
+    tree *P = &T;
     board playingBoard = initBoard();
-    int i = 0;
+    int i = 1;
     while (!isWinningBoard(&playingBoard) && i < 13) {
-        if (i < 6)
+        if (i <= 6)
             printf("====Placement phase====\n");
         else
             printf("====Moving phase====\n");
         char buf[1024];
-        outputBoard(&playingBoard);
-        if (i < 6) {
+        outputTree(P);
+        if (i <= 6) {
             //NOLINTBEGIN(cppcoreguidelines-narrowing-conversions)
             printf("N°%d : Player %d, play your move (0-9) : ", i, playingBoard.turn);
             fgets(buf, 1024,stdin);
             const int pos = strtol(buf, nullptr, 10);
-            if (playMove(&playingBoard, pos))
+            if (playMove(&playingBoard, pos)) {
+                P = P->next[pos];
                 i++;
+            }
             //NOLINTEND(cppcoreguidelines-narrowing-conversions)
         } else {
             //NOLINTBEGIN(cppcoreguidelines-narrowing-conversions)
