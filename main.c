52fe6753bf5d56159ffe1e0c2006ca4e71932cb0
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2024-11-26 20:31:36 +0100

    Added AI (God bless)
    Signed-off-by: Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>

--- main.c
+++ main.c
@@ -6,39 +6,50 @@ typedef struct tree {
     int state[9];
     struct tree *next[9];
 } tree;
+typedef struct pair {
+    int eval;
+    int best_move;
+} pair;
 
-bool isTerminal(const tree *tree) {
+int isTerminal(const tree *tree) {
     for (int i = -1; i < 2; ++i) {
         if (i != 0) {
             for (int j = 0; j < 3; ++j) {
                 if (tree->state[j * 3] == i &&
                     tree->state[j * 3 + 1] == i &&
                     tree->state[j * 3 + 2] == i) {
-                    return true;
+                    return i;
                 }
                 if (tree->state[j] == i &&
                     tree->state[j + 3] == i &&
                     tree->state[j + 6] == i) {
-                    return true;
+                    return i;
                 }
             }
             if (tree->state[0] == i &&
                 tree->state[4] == i &&
                 tree->state[8] == i) {
-                return true;
+                return i;
             }
             if (tree->state[2] == i &&
                 tree->state[4] == i &&
                 tree->state[6] == i) {
-                return true;
+                return i;
             }
         }
     }
-    return false;
+    return 0;
 }
 
+bool isFinal(tree *tree) {
+    for (int i = 0; i < 9; ++i) {
+        if (tree->next[i] != nullptr)
+            return false;
+    }
+    return true;
+}
 tree *nextPlacement(const tree *previous, const int place, const int turn) {
-    if (previous->state[place] != 0 || isTerminal(previous))
+    if (previous->state[place] != 0 || isTerminal(previous) != 0)
         return nullptr;
     tree *P = malloc(sizeof(tree));
     for (int i = 0; i < 9; ++i) {
@@ -64,22 +75,7 @@ tree *nextPlacement(const tree *previous, const int place, const int turn) {
 //     P->state[finalPlace] = turn;
 //     return P;
 // }
-// tree *initTree2(const tree *previous, const int initPlace, const int finalPlace, const int turn, const int n, const int depth) {
-//     tree *P = nextMove(previous, initPlace,finalPlace, turn);
-//     if (n > depth || P == nullptr)
-//         return nullptr;
-//     int t;
-//     for (int i = 0; i < 9; ++i) {
-//         for (int j = 0; j < 10; ++j) {
-//             if (turn == 1)
-//                 t = -1;
-//             else
-//                 t = 1;
-//             P->next[i] = initTree2(P, i,j, t, n + 1, depth);
-//         }
-//     }
-//     return P;
-// }
+
 tree *initTree(const tree *previous, const int place, const int turn, const int n, const int depth) {
     tree *P = nextPlacement(previous, place, turn);
     if (n > depth || P == nullptr)
@@ -131,8 +127,7 @@ void freeAll(tree *P) {
         free(P);
         // if (isTerminal(P)) {
         //     printf("\n=============\n");
-        //     *n = *n +1;
-        //     printf("%d Terminal Board detected :\n", *n);
+        //     printf("Winner : %d Terminal Board detected :\n", isTerminal(P));
         //     outputTree(P);
         //     printf("\n=============\n");
         // }
@@ -197,28 +192,84 @@ bool outputPossibleMove(board *B, int place, int *num, int count) {
     return true;
 }
 
+pair minimax(tree *board, bool maximizing) {
+    pair pair;
+    if (isTerminal(board) == 1) {
+        pair.eval = 1;
+        pair.best_move = -1;
+        return pair;
+    }
+    if (isTerminal(board) == -1) {
+        pair.eval = -1;
+        pair.best_move = -1;
+        return pair;
+    }
+    if (isFinal(board)) {
+        pair.eval = 0;
+        pair.best_move = -1;
+        return pair;
+    }
+    if (maximizing) {
+        int max_eval = -100;
+        int best_move = -1;
+        for (int i = 0; i < 9; ++i) {
+            if (board->next[i] != nullptr) {
+                int eval = minimax(board->next[i], false).eval;
+                if (eval > max_eval) {
+                    max_eval = eval;
+                    best_move = i;
+                }
+            }
+        }
+        pair.eval = max_eval;
+        pair.best_move = best_move;
+        return pair;
+    } else {
+        int min_eval = 100;
+        int best_move = -1;
+        for (int i = 0; i < 9; ++i) {
+            if (board->next[i] != nullptr) {
+                int eval = minimax(board->next[i], true).eval;
+                if (eval < min_eval) {
+                    min_eval = eval;
+                    best_move = i;
+                }
+            }
+        }
+        pair.eval = min_eval;
+        pair.best_move = best_move;
+        return pair;
+    }
+}
+
 int main(void) {
-    // tree *T = makeTree();
-    // tree *P = T;
+    tree *T = makeTree();
+    tree *P = T;
     board playingBoard = initBoard();
     int i = 1;
     int num[3];
     int count = 0;
+    int pos;
+    int startFirst = 0;
     while (!isWinningBoard(&playingBoard) && i < 13) {
         if (i <= 6)
             printf("====Placement phase====\n");
         else
             printf("====Moving phase====\n");
         char buf[1024];
-        // outputTree(P);
-        outputBoard(&playingBoard);
+        outputTree(P);
+        // outputBoard(&playingBoard);
         if (i <= 6) {
             //NOLINTBEGIN(cppcoreguidelines-narrowing-conversions)
-            printf("N°%d : Player %d, play your move (0-9) : ", i, playingBoard.turn);
-            fgets(buf, 1024,stdin);
-            const int pos = strtol(buf, nullptr, 10);
+            if (i % 2 == startFirst)
+                pos = minimax(P, startFirst).best_move;
+            else {
+                printf("N°%d : Player %d, play your move (0-9) : ", i, playingBoard.turn);
+                fgets(buf, 1024,stdin);
+                pos = strtol(buf, nullptr, 10);
+            }
             if (playMove(&playingBoard, pos)) {
-                // P = P->next[pos];
+                P = P->next[pos];
                 i++;
             }
             //NOLINTEND(cppcoreguidelines-narrowing-conversions)
@@ -244,7 +295,7 @@ int main(void) {
         printf("After %d moves, The player %d lost !!!", i, playingBoard.turn);
 
     free(playingBoard.nodes);
-    // freeAll(P);
+    freeAll(P);
     return 0;
 }
 
