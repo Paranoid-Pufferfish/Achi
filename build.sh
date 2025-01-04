22cd445dfed22dc0154a35d488d18617755ede30
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-01-04 16:01:42 +0100

    Did on oopsie, not anymore

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ echo "$1"
 
 (echo "Building for Linux" && cd cmake-build-release && cmake .. $1 && cmake --build . --parallel --clean-first --config Release && cp Achi-console Achi-SDL ../Release/Linux/)
 (echo "Building for Windows" && cd cmake-build-release-windows && cmake .. -DCMAKE_TOOLCHAIN_FILE=../mingw-w64-x86_64.cmake $1 && cmake --build . --parallel --clean-first --config Release && cp Achi-console.exe Achi-SDL.exe ../Release/Windows/)
+cp -rfv media/ Release/
+tar -c -I 'xz -9 -T0' -v -f Release.tar.gz Release/
